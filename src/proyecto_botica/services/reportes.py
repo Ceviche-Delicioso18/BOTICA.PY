e83b5ae6bc6020c67f1ae628dd4d96a271887f43
Antony-Grados18 <giovanni.gradosr@istpargentina.edu.pe>
2025-12-05 11:53:51 -0500

    segundo commit "Define la logica de generacion de reportes y analisis"

---
+++ src/proyecto_botica/services/reportes.py
@@ -0,0 +1,9 @@
+"""
+Modulo de Servicios - Reportes
+Define la logica de generacion de reportes y analisis
+"""
+
+from typing import List, Dict, Any, Optional
+from datetime import datetime
+from ..models.producto import Producto
+from ..repositories.inventario import Inventario
